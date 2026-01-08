078f9687ed8576cc33f14aa6c526faa089341b19
Constantin Völzel <constantin.voelzel@me.thm.de>
2026-01-08 08:34:57 +0100

    Updated version name in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="GEOppi",
-    version="0.0.2",
+    version="0.1.0",
     description="GEOppi is an open-source software tool designed for different processes in the planning and dimensioning stage of heating networks. It makes use of low-level geo-referenced data for network routings (line objects), heat consumers and heat suppliers (polygon objects) and supports planning processes for new heating networks (e.g. in municipal heat planning) as well as the digital representation of existing networks.",
     packages = find_packages(),
     package_data = {

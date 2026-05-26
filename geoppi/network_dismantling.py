6d962d781c81628e535f4acc81219c809fffaf5c
PCCVHOME\const <constantin.voelzel@me.thm.de>
2026-05-26 15:46:20 +0200

    Initial comit for branch VNEP development

---
+++ geoppi/network_dismantling.py
@@ -0,0 +1,314 @@
+# %%
+# -*- coding: utf-8 -*-
+
+
+### Imports
+import numpy as np
+import pandas as pd
+from pathlib import Path
+from tqdm import tqdm
+import geopandas as gp
+from auxFunctions import (assign_attr_by_max_intersection_area, closest_objects_to_points, create_circumferential_points)
+from internal_auxFunctions import (extract_street_names, assign_street_name_2_buildings)
+from line_density_calculation import *
+from create_network_topology import *
+import logging
+
+
+# %%
+
+cs = "EPSG:25832"
+
+### Load data
+flp_in = Path(r"C:\Users\const\Desktop\example_gas_network")
+
+streets = gp.read_file(flp_in / Path(r"ATKIS_DLM50_Straßenachse.gpkg")).to_crs(cs)
+# streets_name = gp.read_file(r"Z:\41_Wärmeplanung_Hessen\06_GIS\ATKIS\Digitales Landschaftsmodell\ATKIS Basis DLM Hessen (shape)\dlm_42002_ax_strasse.shp", encoding ='utf-8').to_crs(cs)
+
+buildings = gp.read_file(flp_in / Path(r"buildings_final.gpkg")).to_crs(cs)
+parcels = gp.read_file(flp_in / Path(r"parcels.gpkg")).to_crs(cs)
+
+gas_network = gp.read_file(flp_in / Path(r"gas_network.gpkg")).to_crs(cs)
+gas_network = gas_network.drop(columns = [n for n in gas_network.columns if not any([k in n.lower() for k in ("nennweite1", "druckstufe", "leitungsfu", "geometry")])]).reset_index(drop = True)
+gas_network["id"] = gas_network.index.copy()
+
+dh_expectation = gp.read_file(flp_in / Path(r"dh_expectation.gpkg")).to_crs(cs)
+
+
+# %%
+## Create convex hull and cut layers to its extent
+hull = gas_network.unary_union.convex_hull
+hull = gp.GeoDataFrame(geometry = [hull], crs = gas_network.crs)
+
+streets = gp.sjoin(streets, hull, how = "inner", predicate  = "intersects")
+streets = streets[~streets["index_right"].isna()].drop(columns = ["index_right"])
+
+# streets_name = gp.sjoin(streets_name, hull, how = "inner", predicate  = "intersects")
+# streets_name = streets_name[~streets_name["index_right"].isna()].drop(columns = ["index_right"])
+
+buildings = gp.sjoin(buildings, hull, how = "inner", predicate  = "intersects")
+buildings = buildings[~buildings["index_right"].isna()].drop(columns = ["index_right"])
+
+parcels = gp.sjoin(parcels, hull, how = "inner", predicate  = "intersects")
+parcels = parcels[~parcels["index_right"].isna()].drop(columns = ["index_right"])
+
+gas_network = gp.sjoin(gas_network, hull, how = "inner", predicate  = "intersects")
+gas_network = gas_network[~gas_network["index_right"].isna()].drop(columns = ["index_right"])
+
+
+# %% Transfer of attributes to gas network lines
+
+## Filter gas network to distribution lines
+mask = [False if any([n in x.lower() for n in ("anschluss",)]) else True for x in gas_network["LEITUNGSFU"]]
+gas_network_distribution = gas_network[mask]
+
+
+## Cut gas network lines with itself
+_, gas_network_distribution = createUniqueJunctions(gas_network_distribution.explode(index_parts = False), splitLines = True)
+
+
+## Transfer street names to gas network sections
+# streets_temp = streets_name.copy()
+# streets_temp.geometry = streets_name.geometry.buffer(5)
+
+# gas_network_distribution = assign_attr_by_max_intersection_area(gp1 = gas_network_distribution, gp_source = streets_temp, attr = ["nam_name", "wdm_widmun"], gp1_id = "id")
+
+
+# %% Assign street name to buildings from address column
+
+buildings["address"] = buildings["address"].replace("null", None)
+buildings["street_name"] = buildings["address"].apply(lambda x: extract_street_names(x))
+
+# %% Assign address of address-buildings to all buildings on same parcel which do not have an address
+
+buildings = assign_street_name_2_buildings(buildings = buildings, parcels = parcels, col_street_name_buildings="street_name", col_street_name_parcels = "lagebeztxt", parcel_id_buildings = "parc_id", uniqueID_parcels = "idflurst")
+
+
+
+# %% Determine Anliegerstraßen (adjacent lines)
+def get_adjacent_lines(
+    polygons:gp.GeoDataFrame,
+    lines:gp.GeoDataFrame,
+    dist:float = 100,
+    nLines:int = 10,
+    resolution:float = 2,
+    blocking_polygons:gp.GeoDataFrame = None,
+    return_connection_lines:bool = False,
+    unique_ID_polys:str = 'unID_polys',
+    unique_ID_lines:str = 'unID_lines',
+    col_adjacentLineIDs:str = 'adjacentLine_IDs',
+    col_adjacentDists:str = 'adjacentDists',
+    col_adjacentConnectionLines:str = 'adjacentConnectionLines'
+    ):
+
+    from auxFunctions import nearest_points
+
+    """
+    Function to determine adjacent lines to provided polygons in such way that either the *nLines* nearest lines within *dist* are returned or only those where the direct connection from polygons to lines does not intersect any object from *parcels*.\n
+
+    :param polygons: GeoDataFrame of polygons to determine adjacent lines for.\n
+    :param lines: GeoDataFrame of lines to determine adjacent lines from.\n
+    :param dist: Distance within which to search for adjacent lines.\n
+    :param nLines: Maximum number of adjacent lines to return.\n
+    :param resolution: Resolution of points along plolygon boundaries.\n
+    :param blocking_polygons: GeoDataFrame of polygons to identify non-adjacent lines (blocking the way).\n
+    :param return_connection_lines: Boolean to return connection lines.\n
+    :param unique_ID_polys: Name of unique ID column in *polygons*.\n
+    :param unique_ID_lines: Name of unique ID column in *lines*.\n
+    :param col_adjacentLineIDs: Name of column to store IDs of adjacent lines.\n
+    :param col_adjacentDists: Name of column to store distances to adjacent lines.\n
+    :return: GeoDataFrame of polygons with adjacent lines as attribute.
+    """
+
+    ### Plausibility checks
+    cs = polygons.crs
+    if cs != lines.crs:
+        lines = lines.copy().to_crs(cs)
+        logging.warning(f'\nAttention! Polygons and lines do not share same crs!')
+
+    if unique_ID_lines not in lines.columns:
+        lines[unique_ID_lines] = np.arange(len(lines))
+    if unique_ID_polys not in polygons.columns:
+        polygons[unique_ID_polys] = np.arange(len(polygons))
+
+    # Create temporary copies and reset indices
+    lines = lines.copy()
+    lines.reset_index(inplace = True, drop = True)
+    dict_idx_idLines = dict(zip(lines.index, lines[unique_ID_lines]))
+
+    ### Create circumferential points around polygons
+    circumferentialPoints = create_circumferential_points(polygons = polygons, distance = resolution)
+    circumferentialPoints[unique_ID_polys] = polygons[unique_ID_polys].copy()
+    circumferentialPoints = circumferentialPoints.explode(index_parts = True).reset_index(drop = True)
+
+    ### Find closest object to each point (indicating index of closest line segment)
+    idxs, distances = closest_objects_to_points(points = circumferentialPoints, geomObjects = lines, nObjects = nLines, maxDist = np.ones(len(circumferentialPoints))*dist)
+        
+    ### Assign nearest line segments to each point at polys' boundaries
+    circumferentialPoints['nearestLines']                            = idxs
+    circumferentialPoints['distanceToLines']                         = distances
+
+    circumferentialPoints["nearestLines_list"] = circumferentialPoints["nearestLines"].apply(lambda x: list(x) if not isinstance(x, list) else x)
+    circumferentialPoints["distanceToLines_list"] = circumferentialPoints["distanceToLines"].apply(lambda x: list(x) if not isinstance(x, list) else x)
+    
+    connectionLines = [[LineString(nearest_points(geom1 = row.geometry, geom2 = lines.iloc[int(k)].geometry)) if k is not None and not (isinstance(k, float) and np.isnan(k)) else None for k in row["nearestLines_list"]] for _, row in circumferentialPoints.iterrows()]
+
+    # connectionLines = [[LineString(nearest_points(geom1 = row.geometry, geom2 = lines.iloc[int(row["nearestLines"])].geometry)) if row["nearestLines"] is not None and not (np.isnan(row["nearestLines"])) else None] for _, row in circumferentialPoints.iterrows()]
+
+    circumferentialPoints["connectionLines"] = connectionLines # Geometries of connection lines
+    
+    # Check lines for intersection with other polygons
+    # If no intersection is found, empty list is returned []
+    if blocking_polygons is not None:        
+
+        circumferentialPoints["blocking_polygons"] = None
+        circumferentialPoints["unblocked_line"] = [[True]*len(j) for j in circumferentialPoints["connectionLines"]]
+        polys_sindex = polygons.sindex
+
+        for idx, cL in circumferentialPoints.iterrows():
+            blocking_polys_ids = []
+
+            own_poly_id = cL[unique_ID_polys]
+
+            valid_gs = cL["unblocked_line"].copy()
+
+            for kk, g in enumerate(cL["connectionLines"]):
+                blocking_polys_ids_single = []
+                
+
+                if g is None or g != g:
+                    continue
+                
+                blocking_polys_candidates_idx = list(polys_sindex.query(g, predicate = "intersects"))
+
+                for ps in blocking_polys_candidates_idx:
+                    poly_row = polygons.iloc[ps]
+
+                    if poly_row[unique_ID_polys] == own_poly_id:
+                        continue
+
+                    intersection = g.intersection(poly_row.geometry)
+
+                    if intersection.length > 0:
+                        blocking_polys_ids_single.append(poly_row[unique_ID_polys])
+                        valid_gs[kk] = False
+
+                blocking_polys_ids.append(blocking_polys_ids_single)
+
+            circumferentialPoints.at[idx, "blocking_polygons"] = blocking_polys_ids
+            circumferentialPoints.at[idx, "unblocked_line"] = valid_gs
+            mask_unblocked = np.array(circumferentialPoints.at[idx, "unblocked_line"])
+            circumferentialPoints.at[idx, "nearestLines_list"] = list(np.array(circumferentialPoints.at[idx, "nearestLines_list"])[mask_unblocked].flatten())
+            circumferentialPoints.at[idx, "distanceToLines_list"] = list(np.array(circumferentialPoints.at[idx, "distanceToLines_list"])[mask_unblocked].flatten())
+            circumferentialPoints.at[idx, "connectionLines"] = list(np.array(circumferentialPoints.at[idx, "connectionLines"])[mask_unblocked].flatten())
+
+    
+    groupedLineIDs = circumferentialPoints.groupby(by = unique_ID_polys)["nearestLines_list"].sum() # Append all entries of line IDs from each circumferential point, grouped by polygon
+    groupedDists = circumferentialPoints.groupby(by = unique_ID_polys)["distanceToLines_list"].sum()    
+    groupedConnectionLines = circumferentialPoints.groupby(by = unique_ID_polys)["connectionLines"].sum()
+
+    breakpoint()
+
+    ### Get unique lines belonging to each polygon
+    adjacentLines, adjacentDists, adjacentConnectionLines = [], [], []
+
+    for nn, ll in enumerate(groupedLineIDs.values):
+        mins = {}
+        lines_res = {}
+        
+        idxnoNaN = ~np.isnan(ll)
+        ll = np.array(ll)[idxnoNaN].astype(int) # Filter out nan line ids
+        dd = np.array(groupedDists.values[nn])[idxnoNaN]
+        cc = np.array(groupedConnectionLines.values[nn])[idxnoNaN] if return_connection_lines else np.full(len(ll), None)[idxnoNaN]
+
+        for idx, (i, d) in enumerate(zip(ll, dd)):
+            if d < mins.get(i, float("inf")):
+                mins[i] = d
+                lines_res[i] = cc[idx]
+
+        if len(mins.keys()) == 0:
+            uniqueLines, shortestDists = tuple(), tuple()
+            uniqueLines_geoms = tuple()
+        else:            
+            uniqueLines, shortestDists = zip(*mins.items())
+            _, uniqueLines_geoms = zip(*lines_res.items())
+
+
+        # Sort unique line IDs, geometries and distances with ascending distance values
+        sort_idx = np.argsort(shortestDists)
+
+        shortestDists = list(np.array(shortestDists)[sort_idx])
+        uniqueLines = list(np.array(uniqueLines)[sort_idx])
+        uniqueLines_geoms = list(np.array(uniqueLines_geoms)[sort_idx])
+
+        adjacentLines.append(uniqueLines)
+        adjacentDists.append(shortestDists)
+        adjacentConnectionLines.append(uniqueLines_geoms)
+
+   
+    # Store in pandas DataFrame
+    if not return_connection_lines and blocking_polygons is None:
+        df_adjacentLines = pd.DataFrame(index = groupedLineIDs.index, data = {col_adjacentLineIDs: adjacentLines, col_adjacentDists: adjacentDists})
+    else:
+        df_adjacentLines = pd.DataFrame(index = groupedLineIDs.index, data = {col_adjacentLineIDs: adjacentLines, col_adjacentDists: adjacentDists, col_adjacentConnectionLines: adjacentConnectionLines})
+
+        df_connectionLines = groupedConnectionLines.copy()
+        df_connectionLines[:] = adjacentConnectionLines # Assign only nObjects shortest geometries of single LineStrings for each building
+        df_connectionLines = df_connectionLines.explode()
+        df_connectionLines = gp.GeoDataFrame(data = {unique_ID_polys:df_connectionLines.index}, geometry = df_connectionLines.values, crs = cs)
+      
+    ### Assign results to polygons
+    polygons = pd.merge(polygons, df_adjacentLines, how = 'left', left_on = unique_ID_polys, right_index = True)
+
+    if return_connection_lines:
+        return polygons, df_connectionLines
+    else:
+        return polygons
+
+
+parcels_out, connLines =  get_adjacent_lines(
+    polygons = parcels.head(50),
+    unique_ID_polys = "idflurst",
+    unique_ID_lines = "line_ID",
+    lines = streets,
+    return_connection_lines = True,
+    blocking_polygons = parcels,
+    resolution = 5,
+    dist = 50,
+    nLines = 5
+)
+
+
+parcels_out['adjacentLine_IDs'] = parcels_out['adjacentLine_IDs'].astype(str)
+parcels_out['adjacentDists'] = parcels_out['adjacentDists'].astype(str)
+# connLines['blocking_polygons'] = connLines['blocking_polygons'].astype(str)
+
+parcels_out.to_file(flp_in / Path(r"parcels_test.gpkg"), driver = "GPKG")
+connLines.to_file(flp_in / Path(r"connectionLines_test.gpkg"), driver = "GPKG")
+# lines_out.to_file(flp_in / Path(r"lines_test.gpkg"), driver = "GPKG")
+
+
+# %% Matching parcel IDs and adjacent line IDs to buildings
+
+# Assign list of adjacent lines to buildings
+buildings_out = pd.merge(buildings, parcels_out[["idflurst", "adjacentLine_IDs", "adjacentDists"]], left_on = "parc_id", right_on = "idflurst", how = "left").drop(columns = ["idflurst"])
+
+
+
+# %%
+
+streets_out, buildings_ld_calc = sum_attributes_on_lines(
+    lines = streets.reset_index(drop = True),
+    polygons = buildings_out,
+    target_attr = "demand_use_th",
+    rand_sampling = False,
+    func_max_distance = lambda x: 200,
+    dict_polyID_lineID = {108:0, 142:1},
+    polygons_uniqueID = "build_ID",
+    lines_uniqueID = "line_ID"
+)
+
+
+
+
+

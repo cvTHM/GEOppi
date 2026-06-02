Finalized implementation of street width calculation function with discrete profiles; Edited yml file (deletion of pre-fixed env save path)

geoppi/examples/streetwidth_example.py
@@ -1,3 +1,5 @@
+# %%
+
 import time
 from pathlib import Path
 import geopandas as gp
@@ -8,10 +10,9 @@ from geoppi import measure_street_width, detect_lines_in_narrow_passages
 _input = Path(r'data/exampleStreetwidth/input')
 _output = Path(r'data/exampleStreetwidth/output')
 
-axis_lines = gp.read_file(_input / 'strassen_buseck.shp')
-street_parcels = gp.read_file(_input / 'flurstuecke_strassenverkehr_buseck.gpkg')
-buildings = gp.read_file(_input / 'flursteucke_buseck_ohne_straßenverkehr.gpkg')
-
+axis_lines = gp.read_file(_input / 'streets.gpkg')
+street_parcels = gp.read_file(_input / 'parcels_traffic.gpkg')
+parcels = gp.read_file(_input / 'parcels.gpkg')
 
 
 print("=" * 60)
@@ -20,24 +21,26 @@ print("=" * 60)
 
 t0 = time.time()
 
-profile_gdf, lines_gdf = measure_street_width(
+axis_lines_gdf, profile_gdf, lines_gdf = measure_street_width(
     axis_lines=axis_lines,
     street_parcels=street_parcels,
     step_size=5.0,
     max_range=30.0,
-    max_valid_width=25.0, # gesamte breite
-    fan_angles=np.linspace(-15, 15, 7).tolist(), # Bsp.: 7 Messlinien im Winkel von -15° bis +15° zur Achse
+    max_valid_width=25.0, # Total width
+    fan_angles=np.linspace(-15, 15, 7).tolist(), # Ex.: 7 measurement line with angle of -15° to +15° rel. to street axis
     overlap_threshold=2,
     min_boundary_angle=70.0,
 )
 
+
 t1 = time.time()
 
-print(f"Dauer: {t1 - t0:.2f}s")
-print(f"Profile-Punkte: {len(profile_gdf)}")
-print(f"Messlinien:     {len(lines_gdf)}")
-profile_gdf.to_file(_output / 'width_profile.gpkg')
-lines_gdf.to_file(_output / 'width_messlinien.gpkg')
+print(f"Duration: {t1 - t0:.2f}s")
+print(f"Profile points: {len(profile_gdf)}")
+print(f"Measurement lines:     {len(lines_gdf)}")
+axis_lines_gdf.to_file(_output / 'width_street_lines.gpkg')
+profile_gdf.to_file(_output / 'width_profile.gpkg', driver = "GPKG")
+lines_gdf.to_file(_output / 'width_messlinien.gpkg', driver = "GPKG")
 print()
 
 
@@ -49,7 +52,7 @@ t0 = time.time()
 
 result_lines, shortest_lines = detect_lines_in_narrow_passages(
     lines=axis_lines,
-    polygons=buildings,
+    polygons=parcels,
     merge_touching_polygons=True,
     threshDistance=10,
     distPointsCircumference=5,
@@ -59,9 +62,9 @@ result_lines, shortest_lines = detect_lines_in_narrow_passages(
 
 t1 = time.time()
 
-print(f"Dauer: {t1 - t0:.2f}s")
-print(f"Linien mit Engstelle: {result_lines['narrowPassage_m'].notna().sum()} / {len(result_lines)}")
-print(f"Kürzeste Verbindungen: {len(shortest_lines)}")
+print(f"Duration: {t1 - t0:.2f}s")
+print(f"Lines with narrow passage: {result_lines['narrowPassage_m'].notna().sum()} / {len(result_lines)}")
+print(f"Shortest connection: {len(shortest_lines)}")
 result_lines = result_lines.drop(columns=['fid'], errors='ignore')
 result_lines.to_file(_output / 'narrow_engstellen.gpkg')
 shortest_lines.to_file(_output / 'narrow_verbindungen.gpkg')